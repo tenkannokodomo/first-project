28123abce57917ad93e6b8d402aa1a7551da1511
tenkannokodomo <sasha2512.dox@gmail.com>
2022-11-17 20:14:33 +0300

    Domashnyaa rabota

--- 123.py
+++ 123.py
@@ -1,9 +1,30 @@
-import time 
 if __name__ == '__main__':
-	words = ['Hui', 'Zalupa', 'Blyat']
-	tm = time.monotonic() + 60 * 30
-	while time.monotonic() < tm:
-		for i in range(len(words)):
-			print(words[i])
-			time.sleep(3)
-	print()
+	words = [22, 1, 12, 36, 14]
+	fruits = ["apple", "raspberry", "banana"]
+
+	words2 = words.copy() #copiya
+
+	words.clear() #chistit spisok
+
+	print(words)
+	print(words2)
+
+	print(fruits.index("raspberry")) #pishet index
+
+	fruits.pop(x) #udalil last x - index
+
+	words.extend(fruits) #soedenyaet spiski words + fruits
+	print(words2.count(33)) #schetaen elementi v spiske
+
+	words2.insert(0, 33) #menyaet 1 element na 33
+
+	x=5
+	y=2
+	print(x/y)
+	print(x//y) #целочисленное деление
+
+	arr = list() #создает пустой список
+
+	len() #длинна массива
+	
+	print(fruit, end=' ') #в строчку чтобы
---
+++ ht.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+	x = int(input('Введите x:'))
+	y = int(input('Vvedite y:'))
+	print(x + y)
